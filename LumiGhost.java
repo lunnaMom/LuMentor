2abf7c8de3cad7ff8d6204ec368240ec261c160b
CODEX_B1TCH <sakuramoon1363@gmail.com>
2025-05-26 21:44:51 +0300

    Core logic and frontend setup
    Web assets + activity injection

---
+++ LumiGhost.java
@@ -0,0 +1,8 @@
+// LumiGhost логика: запись действий, слежка, отправка в Telegram (имитация)
+package com.lumentor.app;
+
+public class LumiGhost {
+    public void trackEverything() {
+        // скрытый код здесь
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+package com.lumentor.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView web = new WebView(this);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("file:///android_asset/index.html");
+        setContentView(web);
+    }
+}
